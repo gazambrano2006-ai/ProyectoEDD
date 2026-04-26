03407e24a0c2a0d3e4d66ee02be47fbbecafa40b
gazambrano2006-ai <gazambrano2006@gmail.com>
2026-04-26 03:18:56 -0500

    Add insertion implementation

---
+++ InsertionSort/MainInsertionSort.java
@@ -0,0 +1,63 @@
+package EI;
+
+import TDAs.DoublyLinkedList;
+
+public class MainInsertionSort {
+    public static void main(String[] args) {
+        DoublyLinkedList<Integer> DouInt = new DoublyLinkedList<>();
+        DoublyLinkedList<String> DouString = new DoublyLinkedList<>();
+        DoublyLinkedList<Float> DouFloat = new DoublyLinkedList<>();
+
+        // Enteros
+        DouInt.addFirst(8);
+        DouInt.addFirst(1);
+        DouInt.addFirst(6);
+        DouInt.addFirst(4);
+
+        // String
+        DouString.addFirst("Hola");
+        DouString.addFirst("adios");
+        DouString.addFirst("Allah");
+
+        // Float
+        DouFloat.addFirst(7.51f);
+        DouFloat.addFirst(5.89f);
+        DouFloat.addFirst(1.15f);
+        DouFloat.addFirst(7.81f);
+
+        // Ordenamiento Enteros
+        for (Integer i : DouInt){
+            System.out.print(i + ", ");
+        }
+        System.out.println(" ");
+        DouInt.insertionSort();
+
+        for (Integer i : DouInt){
+            System.out.print(i + ", ");
+        }
+        System.out.println("\n");
+
+        // Ordenamiento Strings
+        for (String s : DouString){
+            System.out.print(s + ", ");
+        }
+        System.out.println(" ");
+        DouString.insertionSort();
+
+        for (String s : DouString){
+            System.out.print(s + ", ");
+        }
+        System.out.println("\n");
+
+        // Ordenamiento Float
+        for (Float f : DouFloat){
+            System.out.print(f + ", ");
+        }
+        System.out.println(" ");
+        DouFloat.insertionSort();
+
+        for (Float f : DouFloat){
+            System.out.print(f + ", ");
+        }
+    }
+}
